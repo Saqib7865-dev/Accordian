117c8c670808f12ec486e9e0c5fd311ef0c7abf8
Muhammad Saqib Ali <abc@gmail.com>
2024-07-31 19:35:55 +0500

    Added multi selection functionality

--- src/components/accordian/index.jsx
+++ src/components/accordian/index.jsx
@@ -1,14 +1,27 @@
 import React, { useState } from "react";
-import './style.css'
+import "./style.css";
 import data from "./data";
 
 const Accordian = () => {
   const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
   function handleSingleSelection(getCurrentId) {
-    setSelected(getCurrentId===selected?null:getCurrentId);
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+  function handleMultiSelection(id) {
+    let copyMultiple = [...multiple];
+    const findIndexOfCurrentId = copyMultiple.indexOf(id);
+    findIndexOfCurrentId === -1
+      ? copyMultiple.push(id)
+      : copyMultiple.splice(findIndexOfCurrentId, 1);
+    setMultiple(copyMultiple);
   }
   return (
     <div className="wrapper">
+      <button onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+        Enable Multi Selection
+      </button>
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => {
@@ -16,14 +29,26 @@ const Accordian = () => {
               <div className="item" key={dataItem.id}>
                 <div
                   className="title"
-                  onClick={() => handleSingleSelection(dataItem.id)}
+                  onClick={
+                    enableMultiSelection
+                      ? () => handleMultiSelection(dataItem.id)
+                      : () => handleSingleSelection(dataItem.id)
+                  }
                 >
                   <h3>{dataItem.question}</h3>
                   <span>+</span>
                 </div>
-                {selected === dataItem.id ? (
+                {enableMultiSelection ? (
+                  multiple.indexOf(dataItem.id) !== -1 ? (
+                    <div className="content">{dataItem.answer}</div>
+                  ) : (
+                    null
+                  )
+                ) : selected === dataItem.id ? (
                   <div className="content">{dataItem.answer}</div>
-                ) : null}
+                ) : (
+                  null
+                )}
               </div>
             );
           })
